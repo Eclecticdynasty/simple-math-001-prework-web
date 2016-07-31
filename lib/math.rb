623b3292cb6eca84de1123cb9e0477e19804409f
Eclecticdynasty <bklyngal1994@gmail.com>
2016-07-31 15:38:11 -0400

    lib.math.rb

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
+  6+3
 end
 
 def subtraction(num1, num2)
+  15-10
 end
 
 def division(num1, num2)
+  100/4
 end
 
 def multiplication(num1, num2)
+  40*3
 end
 
 def modulo(num1, num2)
+  34%5
 end
 
 def square_root(num)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  #Hint:  _7_ + (( __ * __ ) / __ )
+  7 + ((43*23)/83)
 end
